55098500942329d60c250c6c929c89e6a4d48995
Abhijeet kumar <abhijeet122kumar@gmail.com>
2025-01-15 18:12:40 +0530

    working on whatsapp button

---
+++ src/pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+        <script defer type="text/javascript" src="https://sony.community.druidplatform.com/v2/druid_webchat_v2_load.js?botId=2f002613-b12e-41bf-ed29-08dd300fe1ee&amp;baseUrl=https%3A%2F%2Fdruidapi.comm.eu.druidplatform.com">
+        </script>
+      </body>
+    </Html>
+  );
+}
